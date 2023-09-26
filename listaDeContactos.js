e4e8fcdd8a0e6380faa4e10c9e11994bb12f6257
Danny <danny22marin@gmail.com>
2023-09-25 23:15:28 -0500

    Review-1

--- listaDeContactos.js
+++ listaDeContactos.js
@@ -1,3 +1,4 @@
+//Lista de contactos (Array de objetos).
 const listaContactos = [
   {
     id: 100001,
@@ -36,18 +37,43 @@ const listaContactos = [
   },
 ];
 
+//Agregar un contacto.
 let agregar = function (cont) {
   listaContactos.push(cont);
 };
 
+//Borrar un contacto.
 let borrar = function (ind, cant) {
   listaContactos.splice(ind, cant);
 };
 
+//Imprimir lista de contactos.
 let imprimirLista = function () {
   for (contacto of listaContactos) console.log(contacto);
 };
 
+//Actualizar un contacto.
+let actualizarContacto = function (indice, value) {
+  let indx;
+  for (let i = 0; i < listaContactos.length; i++) {
+    if (indice === listaContactos[i].id) {
+      indx = i;
+      break;
+    }
+  }
+
+  if (indx >= 0) {
+    let nuevaLista = Object.assign(listaContactos[indx], value);
+    console.log(
+      `El contacto ${listaContactos[indx].nombres} con id ${indice} fue actualizado correctamente`
+    );
+    console.log("Usuario actualizado", nuevaLista);
+  } else {
+    console.log("Contacto no encontrado");
+  }
+};
+
+//Uso de funciones.
 agregar({
   id: 100006,
   nombres: "Danny Alexander",
@@ -61,3 +87,5 @@ borrar(3, 1);
 console.log(listaContactos);
 
 imprimirLista();
+
+actualizarContacto(100002, { nombres: "Henry Luis" });
